b6f7dd3a879d11a8cc30f3a427b84d272ee13efc
viral32111 <19510403+viral32111@users.noreply.github.com>
2023-05-20 23:02:04 +0100

    IDE said so

--- src/main/kotlin/com/viral32111/askai/AskAI.kt
+++ src/main/kotlin/com/viral32111/askai/AskAI.kt
@@ -1,14 +1,12 @@
 package com.viral32111.askai
 
 import net.fabricmc.api.ModInitializer
-import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 @Suppress( "UNUSED" )
 object AskAI: ModInitializer {
 	private const val MOD_ID = "ask-ai"
-
-	val LOGGER: Logger = LoggerFactory.getLogger( "ask-ai" )
+	private val LOGGER = LoggerFactory.getLogger( "ask-ai" )
 
 	override fun onInitialize() {
 		LOGGER.info( "Ask AI has been initialized." )
